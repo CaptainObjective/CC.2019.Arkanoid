f9f2b0c932d49daa277108f5cbf0ed20cda2d5ed
Piotr Bocian <piotr.bocian.96@gmail.com>
2019-08-15 21:23:15 +0200

    Ciegiełki na polu i dodana klasa bricks.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Paddle from './paddle';
 import Ball from './ball';
+import Brick from './brick';
 
 export const canvas = document.querySelector('canvas');
 export const ctx = canvas.getContext('2d');
@@ -8,12 +9,21 @@ export const ch = canvas.height;
 
 const paddle = new Paddle(cw / 2);
 const ball = new Ball(paddle.x + paddle.length / 2, paddle.height);
-
+const bricks=[];
+for( let i = 0; i<=3;i++){
+for(let j = 0; j<=8;j++){
+  const brick = new Brick(30+j*60,270-i*40);
+  bricks.push(brick);
+}
+}
 const gameLoop = () => {
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, cw, ch); //tło
   ball.move(0, 0);
   ball.onHit();
+  for(let i = 0; i<bricks.length;i++){
+  bricks[i].draw();
+  }
   paddle.draw();
   ball.draw();
   requestAnimationFrame(gameLoop);
