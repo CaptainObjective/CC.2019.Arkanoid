263474b4935611e2ec13bc1600c48ca250b5273c
Patryqss <gpatryk14@gmail.com>
2019-08-17 17:12:40 +0200

    Dodany shrinkBall

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Paddle from './paddle';
 import Ball from './ball';
 import enLargeBall from './powerUPs/enLargeBall'
+import shrinkBall from './powerUPs/shrinkBall';
 
 export const canvas = document.querySelector('canvas');
 export const ctx = canvas.getContext('2d');
@@ -10,6 +11,7 @@ export const ch = canvas.height;
 const paddle = new Paddle(cw / 2);
 const ball = new Ball(paddle.x + paddle.length / 2, paddle.height);
 const LargeBall = new enLargeBall(-100, -100);
+const SmallBall = new shrinkBall(-100, -100);
 
 const gameLoop = () => {
   ctx.fillStyle = 'black';
@@ -23,6 +25,10 @@ const gameLoop = () => {
   LargeBall.fall();
   LargeBall.hitPaddle(ball, paddle, LargeBall);
 
+  SmallBall.draw();
+  SmallBall.fall();
+  SmallBall.hitPaddle(ball, paddle, SmallBall);
+
   requestAnimationFrame(gameLoop);
 };
 
@@ -30,9 +36,18 @@ document.addEventListener('click', e => {
   console.log(e);
   ball.ySpeed = -5;
   ball.move();
+});
+document.addEventListener('keydown', e => {
+  console.log(e);
   //powrot powerupa na ekran
   LargeBall.x = cw / 2 + 10;
   LargeBall.y = ch / 2
+});
+document.addEventListener('keyup', e => {
+  console.log(e);
+  //powrot powerupa na ekran
+  SmallBall.x = cw / 2 + 10;
+  SmallBall.y = ch / 2
 });
 
 requestAnimationFrame(gameLoop);
