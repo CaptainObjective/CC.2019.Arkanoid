d71f1425cd4f14a016604be1438c0bd218dcdf5a
Piotr Bocian <piotr.bocian.96@gmail.com>
2019-08-19 21:26:50 +0200

    Losowanie i nieszczenie cegiełek

---
+++ src/CC.2019.Arkanoid-PowerUps/src/powerUPs/shrinkBall.js
@@ -0,0 +1,39 @@
+import { ctx, cw, ch } from '../main';
+import PowerUp from '../powerUp';
+
+class shrinkBall extends PowerUp {
+    constructor(x,y) {
+        super(x,y);
+        this.size = 5;
+    }
+    draw() {
+        ctx.fillStyle = 'red';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI*2, true);
+        ctx.lineTo(this.x, this.y-10);
+        ctx.lineTo(this.x-6, this.y-14);
+        ctx.lineTo(this.x+6, this.y-14);
+        ctx.lineTo(this.x, this.y-10);
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x, this.y+10);
+        ctx.lineTo(this.x-6, this.y+14);
+        ctx.lineTo(this.x+6, this.y+14);
+        ctx.lineTo(this.x, this.y+10);
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x-10, this.y);
+        ctx.lineTo(this.x-14, this.y-6);
+        ctx.lineTo(this.x-14, this.y+6);
+        ctx.lineTo(this.x-10, this.y);
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x+10, this.y);
+        ctx.lineTo(this.x+14, this.y-6);
+        ctx.lineTo(this.x+14, this.y+6);
+        ctx.lineTo(this.x+10, this.y);
+        ctx.fill();
+    }
+    workWithBall (ball) {
+        if (ball.size>3) ball.size *= 0.8;
+    }
+}
+
+export default shrinkBall;
---
+++ src/CC.2019.Arkanoid-PowerUps/src/powerUp.js
@@ -0,0 +1,25 @@
+import { ctx, cw, ch } from './main';
+
+class PowerUp {
+  constructor(x, y) {
+    this.ySpeed = 3;
+    this.x = x;
+    this.y = ch - y;
+    this.protect = false;
+  }
+  fall() {
+    this.y += this.ySpeed;
+  }
+  work() {}
+  workWithBall() {}
+  workWithPaddle() {}
+  hitPaddle(ball, paddle) { 
+    if (this.y - (ch-paddle.height-paddle.spaceFromBorder) > 0 && this.x >= paddle.position.x && this.x <= paddle.position.x + paddle.length) {
+      this.work();
+      this.workWithBall(ball);
+      this.workWithPaddle(paddle);
+      return true;
+    }
+  }
+}
+export default PowerUp;
