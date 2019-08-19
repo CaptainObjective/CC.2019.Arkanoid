4feeba9e353d4364d2756d806d55d0e7b726cee9
Unknown <marcinwos18@gmail.com>
2019-08-19 21:29:56 +0200

    bugi po mergu i eslint

--- src/main.js
+++ src/main.js
@@ -16,13 +16,13 @@ export const cw = canvas.width;
 export const ch = canvas.height;
 
 const paddle = new Paddle();
-const ball = new Ball(paddle.position.x + paddle.length / 2, paddle.height+paddle.spaceFromBorder);
-const bricks=[];
+const ball = new Ball(paddle.position.x + paddle.length / 2, paddle.height + paddle.spaceFromBorder);
+const bricks = [];
 const powerups = [];
 let chain;
-for( let i = 0; i<=3;i++){
-  for(let j = 0; j<=8;j++){
-    const brick = new Brick(30+j*60,270-i*40);
+for (let i = 0; i <= 3; i++) {
+  for (let j = 0; j <= 8; j++) {
+    const brick = new Brick(30 + j * 60, 270 - i * 40);
     bricks.push(brick);
   }
 }
@@ -51,15 +51,14 @@ const gameLoop = () => {
       if (powerup.hitPaddle(ball, paddle)) {
         powerups.shift();
       }
-      if (powerup.y>ch) powerups.shift();
+      if (powerup.y > ch) powerups.shift();
       if (powerup.protect) {
         chain = new rescueChainObj();
         console.log('yay');
       }
     }
   }
-  if (chain)  chain.draw();
-  requestAnimationFrame(gameLoop);
+  if (chain) chain.draw();
 
   //Game end check
   if (ball.outOfCanvas()) {
@@ -84,17 +83,25 @@ document.addEventListener('keydown', ({ keyCode }) => {
       paddle.setDir('RIGHT');
       break;
   }
+});
 
 //Tworzenie powerupów w ramach testów, do poźniejszego usunięcia
 document.addEventListener('keydown', e => {
-  if (event.keyCode===97)  powerups.push(new enLargeBall(cw / 2 + 10, ch / 2)); //numpad1
-  else if (event.keyCode===98)  powerups.push(new shrinkBall(cw / 2 + 10, ch / 2)); //numpad2
-  else if (event.keyCode===99)  powerups.push(new enLargePaddle(cw / 2 + 10, ch / 2)); //numpad3
-  else if (event.keyCode===100)  powerups.push(new shrinkPaddle(cw / 2 + 10, ch / 2)); //numpad4
-  else if (event.keyCode===101)  powerups.push(new speedUpBall(cw / 2 + 10, ch / 2)); //numpad5
-  else if (event.keyCode===102)  powerups.push(new speedDownBall(cw / 2 + 10, ch / 2)); //numpad6
-  else if (event.keyCode===103)  powerups.push(new rescueChain(cw / 2 + 10, ch / 2)); //numpad7
-  else if (event.keyCode===104) chain=null //numpad8, normalnie znika po zderzeniu z piłką
+  if (event.keyCode === 97) powerups.push(new enLargeBall(cw / 2 + 10, ch / 2));
+  //numpad1
+  else if (event.keyCode === 98) powerups.push(new shrinkBall(cw / 2 + 10, ch / 2));
+  //numpad2
+  else if (event.keyCode === 99) powerups.push(new enLargePaddle(cw / 2 + 10, ch / 2));
+  //numpad3
+  else if (event.keyCode === 100) powerups.push(new shrinkPaddle(cw / 2 + 10, ch / 2));
+  //numpad4
+  else if (event.keyCode === 101) powerups.push(new speedUpBall(cw / 2 + 10, ch / 2));
+  //numpad5
+  else if (event.keyCode === 102) powerups.push(new speedDownBall(cw / 2 + 10, ch / 2));
+  //numpad6
+  else if (event.keyCode === 103) powerups.push(new rescueChain(cw / 2 + 10, ch / 2));
+  //numpad7
+  else if (event.keyCode === 104) chain = null; //numpad8, normalnie znika po zderzeniu z piłką
 });
 
 document.addEventListener('keyup', ({ keyCode }) => {
@@ -106,6 +113,7 @@ document.addEventListener('keyup', ({ keyCode }) => {
       paddle.setDir(null);
       break;
   }
+});
 document.addEventListener('keyup', e => {
   console.log(powerups); //do sprawdzenia aktywnych powerupów
 });
